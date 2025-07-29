1e4ebf7090195fd7b641024ab15435b6813a4c56
Kévin Dahmani <kevin-externe.dahmani@enedis.fr>
2025-07-29 11:48:42 +0200

    PhotorEditor for Android
    Dessin libre
    text
    carré
    cercle
    flèche
    ligne
    choix de la couleur

---
+++ android/src/main/java/fr/binova/capacitor/photoeditor/PhotoEditor.kt
@@ -0,0 +1,51 @@
+package fr.binova.capacitor.photoeditor
+
+import android.app.Activity
+import android.content.Intent
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import com.getcapacitor.JSObject
+import com.getcapacitor.Plugin
+import com.getcapacitor.PluginCall
+import com.getcapacitor.PluginMethod
+import com.getcapacitor.annotation.CapacitorPlugin
+
+@CapacitorPlugin(name = "PhotoEditor")
+class PhotoEditor : Plugin() {
+    private var launcher: ActivityResultLauncher<Intent?>? = null
+    private var savedCall: PluginCall? = null
+
+    override fun load() {
+        launcher = bridge.registerForActivityResult(
+            StartActivityForResult()
+        ) { result: ActivityResult ->
+            savedCall?.let { call ->
+                if (result.resultCode == Activity.RESULT_OK && result.data != null) {
+                    val editedImagePath = result.data?.getStringExtra("editedImagePath")
+                    val ret = JSObject().apply {
+                        put("image", editedImagePath)
+                    }
+                    call.resolve(ret)
+                } else {
+                    call.reject("Photo editing cancelled or failed.")
+                }
+            }
+        }
+    }
+
+
+    @PluginMethod
+    fun editPhoto(call: PluginCall) {
+        val imagePath = call.getString("image")
+        if (imagePath == null || imagePath.isEmpty()) {
+            call.reject("Image path is required.")
+            return
+        }
+
+        val intent = Intent(context, PhotoEditorActivity::class.java)
+        intent.putExtra("imagePath", imagePath)
+        savedCall = call
+        launcher!!.launch(intent)
+    }
+}
---
+++ android/src/main/java/fr/binova/capacitor/photoeditor/TextViewModel.kt
@@ -0,0 +1,13 @@
+package fr.binova.capacitor.photoeditor
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+
+//class TextViewModel : ViewModel() {
+//
+//    var text = MutableLiveData<String>()
+//
+//
+//
+//}
